8ddda0b3ee7c3a73231e2d0f017892e2e74d50ef
ziginfo <34913387+ziginfo@users.noreply.github.com>
2025-08-31 13:10:05 +0200

    Add files via upload

---
+++ mix16ShowCue/README.md
@@ -0,0 +1,9 @@
+## Remote Layout for MiX16 ShowCue and Open Stage Control
+This is a template for the MiX16 App : [ShowCuePro](https://mix16showcue.com/) for OpenStageControl
+---
+OpenStageControl can be downloaded on their [Website](https://openstagecontrol.ammd.net/)...it's Open-Source and free software  
+
+---
+OSC-Control must be activated in the Show-Cue parameters-setup !   
+The OSC INPUT and OUTPUT Ports must be set correctly in both : OpenstageControl and MiX16 Show Cue ! If both are on the same computer the IP-Address should be : 127.0.0.1
+ 
---
+++ mix16ShowCue/module.js
@@ -0,0 +1,81 @@
+// Open Stage Control Custom Module
+
+const cueAddresses = ['/mix16showcue/nextcue/name', '/mix16showcue/playingcue/name'];
+
+module.exports = {
+    init: () => {
+        const remote = settings.read('send')[0];
+        const host = remote.split(':')[0];
+        const port = remote.split(':')[1];
+
+        function refreshCues() {
+            for (const a of cueAddresses) {
+                send(host, port, a);
+            }
+        }
+        function refreshPatch() {
+            send(host, port, '/settings/light/patch');
+        }
+        function thump() {
+            send(host, port, '/thump');
+        }
+
+        setInterval(refreshCues, 0.5 * 1000);
+        setInterval(refreshPatch, 5 * 1000);
+        setInterval(thump, 30 * 1000);
+    },
+    oscInFilter: (data) => {
+        if (data.address.startsWith('/reply')) {
+            const regex = /^\/reply\/cue_id\/[A-Z0-9\-]+\/([a-zA-Z\/]+)$/;
+            if (regex.test(data.address)) {
+                data.address = data.address.replace(regex, '/cue/selected/$1');
+            } else {
+                data.address = data.address.replace(/^\/reply/, '');
+            }
+
+            const reply = JSON.parse(data.args[0].value);
+            if (reply.workspace_id) {
+                receive(
+                    '192.168.50.49',
+                    7201,
+                    '/workspace_id',
+                    reply.workspace_id
+                );
+            }
+
+            data.args = [
+                {
+                    type: 's',
+                    value: reply.data,
+                },
+            ];
+        }
+
+        return data;
+    },
+    oscOutFilter: (data) => {
+        if (data.args.some((a) => a.value === 'rgb')) {
+            const index = data.args.findIndex((a) => a.value === 'rgb');
+            const [r, g, b] = data.args
+                .slice(index + 1, index + 4)
+                .map((a) => a.value);
+            data.args = data.args.slice(0, index);
+            data.args.push({
+                type: 's',
+                value: `rgb(${r},${g},${b})`,
+            });
+        } else if (data.args.some((a) => a.value === 'pantilt')) {
+            const index = data.args.findIndex((a) => a.value === 'pantilt');
+            const [pan, tilt] = data.args
+                .slice(index + 1, index + 3)
+                .map((a) => a.value);
+            data.args = data.args.slice(0, index);
+            data.args.push({
+                type: 's',
+                value: `pantilt(${pan},${tilt})`,
+            });
+        }
+
+        return data;
+    },
+};

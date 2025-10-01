106e6a014ff852dc871f2175be5d5fabf5bec866
ziginfo <34913387+ziginfo@users.noreply.github.com>
2025-10-01 09:59:18 +0200

    Add files via upload

--- AbletonLiveOSC/live_module.js
+++ AbletonLiveOSC/live_module.js
@@ -4,6 +4,7 @@ const lbl = {};
 const host = '127.0.0.1';
 const port = 11000 ;
 const names = {};
+const track_names = [];
 const selno = 0;
 const hex_colors_id =[
 "#FF94A6","#FFA529","#CC9927","#F7F47C","#BFFB00","#1AFF2F","#25FFA8","#5CFFE8","#8BC5FF","#5480E4","#92A7FF","#D86CE4","#E553A0","#FFFFFF","#FF3636","#F66C03","#99724B","#FFF034","#87FF67","#3DC300","#00BFAF","#19E9FF","#10A4EE","#007DC0","#886CE4","#B677C6","#FF39D4","#D0D0D0","#E2675A","#FFA374","#D3AD71","#EDFFAE","#D2E498","#BAD074","#9BC48D","#D4FDE1","#CDF1F8","#B9C1E3","#CDBBE4","#AE98E5","#E5DCE1","#A9A9A9","#C6928B","#B78256","#99836A","#BFBA69","#A6BE00","#7DB04D","#88C2BA","#9BB3C4","#85A5C2","#8393CC","#A595B5","#BF9FBE","#BC7196","#7B7B7B","#AF3333","#A95131","#724F41","#DBC300","#85961F","#539F31","#0A9C8E","#236384","#1A2F96","#2F52A2","#624BAD","#A34BAD","#CC2E6E","#3C3C3C"
@@ -12,7 +13,7 @@ const hex_colors_id =[
 /* this works (with msec) :
 
  setTimeout (function(){
-      set('widget', 'value) 
+      set('widget', 'value') 
       }, 1000);
       
        setTimeout (function(){
@@ -50,6 +51,12 @@ module.exports = {
 //====================================================================
 
     oscInFilter:function(data){
+    
+    
+    	 // receive incoming osc messages in the console
+
+//    receive('/LOG', `MSG IN: ${data.address}, ${data.args.map(a=>a.value).join(', ')}`)
+    	
         // Filter incoming osc messages
 
         var {address, args, host, port} = data
@@ -59,6 +66,9 @@ module.exports = {
           	
         if (address == '/live/track/get/send') {
           	address = '/live/track/set/send' }
+          	
+        if (address == '/live/track/get/panning') {
+        	address = '/live/track/set/panning' }
           	          
         if (address == '/live/track/get/mute') {
         	address = '/live/track/set/mute'
@@ -72,6 +82,21 @@ module.exports = {
         	
         if (address == '/live/track/get/arm') {       
           	address = '/live/track/set/arm' 
+        	if (data.args[1].value== true) {data.args[1].value= 1}
+        	else {data.args[1].value= 0}  }
+        	
+        if (address == '/live/track/get/is_visible') {       
+          	address = '/live/track/get/is_visible' 
+        	if (data.args[1].value== true) {data.args[1].value= 1}
+        	else {data.args[1].value= 0}  }
+        	
+        	if (address == '/live/track/get/is_grouped') {       
+          	address = '/live/track/get/is_grouped' 
+        	if (data.args[1].value== true) {data.args[1].value= 1}
+        	else {data.args[1].value= 0}  }
+        	
+        	if (address == '/live/track/get/is_foldable') {       
+          	address = '/live/track/get/is_foldable' 
         	if (data.args[1].value== true) {data.args[1].value= 1}
         	else {data.args[1].value= 0}  }
         	       	
@@ -91,6 +116,11 @@ module.exports = {
         	var seltr = data.args[0].value 
         	}
         	
+        if (address == '/live/view/get/selected_scene') {
+        	var selsc = data.args[0].value 
+//        	receive('/selscene1',selsc)
+        	}	
+                	
         if (address == '/live/song/get/is_playing') {
       		var is = data.args[0].value 
       		if(is == true){
@@ -136,6 +166,11 @@ module.exports = {
 //====================================================================
 
     oscOutFilter:function(data){
+    
+    	// Filter outgoing osc messages
+    
+    receive('/LOG', `MSG OUT: ${data.address}, ${data.args.map(a=>a.value).join(', ')}`)
+    	
         // Filter outgoing osc messages
 
         var {address, args, host, port, clientId} = data
